069a53d417bd0d4ccec1b37015c722f2d8e2d7a8
device-id <92888249+device-id@users.noreply.github.com>
2025-06-29 16:50:47 -0400

    add base plugin, will automatically get copied over to the ida plugin directory

---
+++ plugin.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+#include <ida.hpp>
+#include <idp.hpp>
+#include <loader.hpp>
+#include <funcs.hpp>
+
+
+// Define the class that inherits from plugmod_t
+class MyPlugmod : public plugmod_t
+{
+public:
+    // Constructor
+    MyPlugmod()
+    {
+        msg("MyPlugmod: Constructor called.\n");
+    }
+
+    // Destructor
+    virtual ~MyPlugmod()
+    {
+        msg("MyPlugmod: Destructor called.\n");
+    }
+
+    // Method that gets called when the plugin is activated
+    virtual bool idaapi run(size_t arg) override
+    {
+        msg("MyPlugmod.run() called with arg: %zu\n", arg);
+
+        // Add some actual functionality to test
+        msg("Plugin is working! Current database: %s\n", get_path(PATH_TYPE_IDB));
+
+        return true;
+    }
+};
+
+static plugmod_t* idaapi init(void)
+{
+    msg("Plugin init() called\n");
+    return new MyPlugmod();
+}
+
+static void idaapi term(void)
+{
+    msg("Plugin term() called\n");
+}
+
+static bool idaapi run(size_t arg)
+{
+    msg("Plugin run() called with arg: %zu\n", arg);
+    return false; // This shouldn't be called with PLUGIN_MULTI
+}
+
+plugin_t PLUGIN = {
+    IDP_INTERFACE_VERSION,
+    PLUGIN_MULTI,         // plugin flags
+    init,                 // initialize
+    term,                 // terminate. this pointer can be nullptr
+    run,                  // invoke the plugin
+    "List functions plugin", // long comment about the plugin
+    "This plugin demonstrates basic functionality", // multiline help about the plugin
+    "List functions",     // the preferred short name of the plugin
+    "Ctrl-Shift-L"        // the preferred hotkey to run the plugin
+};
